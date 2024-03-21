b065bf83ee5d15ea004bbbef8b667c42151d0128
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-03-22 05:31:55 +0600

    dark mode set by local stoarge

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Nav from "../components/Nav";
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <h1>Home page</h1>
+
+        </div>
+    );
+};
+
+export default Home;
